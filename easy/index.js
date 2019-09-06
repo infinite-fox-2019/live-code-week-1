98a22a6e075f4fdb3cd0308a0732932972c5aea3
adityain98 <adityain98@gmail.com>
2019-09-06 10:07:52 +0700

    commit 1

--- easy/index.js
+++ easy/index.js
@@ -1,13 +1,39 @@
 // RELEASE 0
 function makeSolvent() {
   const availableIngredients = 'CFHOP';
-  // code here
-}
+  let solvent = availableIngredients[Math.floor(Math.random() * availableIngredients.length)];
+  let random = 4 + Math.floor(Math.random() * 6);
 
+  for(let i = 0; i<random; i++){
+    solvent += availableIngredients[Math.floor(Math.random() * availableIngredients.length)];
+  }
+
+  return solvent;
+}
 // RELEASE 1
 function checkTotalEnergy(solvent) {
-  // code here
+  let totalH = 0;
+  let totalO = 0;
+
+  for(let i = 0; i<solvent.length; i++){
+    if(solvent[i] === 'H'){
+      totalH ++;
+    }
+    if(solvent[i] === 'O'){
+      totalO ++;
+    }
+  }
+
+  let energy = totalH / 2;
+  if(totalO > 0){
+    if(totalH > totalO){
+      
+    }
+  }
+
 }
+
+checkTotalEnergy(makeSolvent())
 
 // RELEASE 2
 function waterSupply(needEnergy) {
@@ -19,4 +45,4 @@ function aggregate(solvent) {
   // code here
 }
 
-console.log(waterSupply(3));
+// console.log(waterSupply(3));
--- hard/index.js
+++ hard/index.js
@@ -1,66 +1,111 @@
-// STATIC Stage Information
-const stage1 = 'oooooooooooooooo';
-const lengthStage1 = 4;
-const stage2 = 'oooooooooooooooooxoo';
-const lengthStage2 = 5;
-const stage3 = 'oxooxooxooxo';
-const lengthStage3 = 3;
-const stage4 = 'ooooooooooxooooooxxooooxxxooooxxxoooxxxxxo';
-const lengthStage4 = 7;
-const jumpPowerStage1 = 0;
-const jumpPowerStage2 = 3;
-const jumpPowerStage3 = 6;
-const jumpPowerStage4 = 2;
-// STATIC Stage End
+// // STATIC Stage Information
+// const stage1 = 'oooooooooooooooo';
+// const lengthStage1 = 4;
+// const stage2 = 'oooooooooooooooooXoo';
+// const lengthStage2 = 5;
+// const stage3 = 'oXooXooXooXo';
+// const lengthStage3 = 3;
+// const stage4 = 'ooooooooooXooooooXXooooXXXooooXXXoooXXXXXo';
+// const lengthStage4 = 7;
+// const jumpPowerStage1 = 0;
+// const jumpPowerStage2 = 3;
+// const jumpPowerStage3 = 6;
+// const jumpPowerStage4 = 2;
+// // STATIC Stage End
 
-function printBoard(strTrack, lengthTrack) {
-  // code here
-}
+// function printBoard(strTrack, lengthTrack) {
+//   let arrOut = [];
+//   let arrIn = [];
+//   let count = 0;
+//   let obstacles = []
+//   for(let i = 0; i<strTrack.length; i += lengthTrack){
+//     arrIn = [];
+//     for(let j = 0; j<lengthTrack ; j++){
+//       if(strTrack[i + j] == 'o'){
+//         arrIn.push(' ')
+//       }
+//       else if(strTrack[i + j] == 'X'){
+//         obstacles.push(count, j)
+//         arrIn.push(strTrack[i + j])
+//       }
+//     }
+//     count++;
+//     arrOut.push(arrIn)
+//   }
+//   return arrOut
+// }
 
-const boards1 = printBoard(stage1, lengthStage1);
-const boards2 = printBoard(stage2, lengthStage2);
-const boards3 = printBoard(stage3, lengthStage3);
-const boards4 = printBoard(stage4, lengthStage4);
+// const boards1 = printBoard(stage1, lengthStage1);
+// const boards2 = printBoard(stage2, lengthStage2);
+// const boards3 = printBoard(stage3, lengthStage3);
+// const boards4 = printBoard(stage4, lengthStage4);
 
-// RELEASE 0
-// console.log(boards1);
-// console.log(boards2);
-// console.log(boards3);
-// console.log(boards4);
+// // RELEASE 0
+// // console.log(boards1);
+// // console.log(boards2);
+// // console.log(boards3);
+// // console.log(boards4);
 
-function marioLastPosition(boards, jumpPower) {
-  // code here
-}
+// function marioLastPosition(boards, jumpPower) {
+//   let marioRow = boards.length -1;
+//   let marioCol = boards[boards.length - 1].length - 1;
+//   let selesai = false;
+//   let jumping = 0;
 
-// RELEASE 1
+//   while(!selesai){
+//     if(boards[marioRow][marioCol+1] == ' '){
+//       marioCol ++;
+//       jumping = 0;
+//     }
+//     else if(boards[marioRow][marioCol+1] == 'X') {
+//       if(jumping <jumpPower){
+//         marioRow -= 1;
+//         jumping ++
+//       }
+//       else{
+//         return 'Mario Gagal'
+//       }
+//     }
+//   }
+//   return boards;
+// }
+
+// // RELEASE 1
 // const marioLastPos1 = marioLastPosition(boards1, jumpPowerStage1);
 // const marioLastPos2 = marioLastPosition(boards2, jumpPowerStage2);
 // const marioLastPos3 = marioLastPosition(boards3, jumpPowerStage3);
 
-function clearScreen() {
-  // Un-comment this line if you have trouble with console.clear();
-  // return process.stdout.write('\033c');
-  console.clear();
-}
-
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
-    }
-  }
-}
-
-function animate(boards, jumpPower) {
-  // code here
-}
+// console.log(marioLastPos1);
 
 
-// RELEASE 2
-// animate(boards1, jumpPowerStage1);
-// RELEASE 3
-// animate(boards2, jumpPowerStage2);
-// animate(boards3, jumpPowerStage3);
-// RELEASE 4
-// animate(boards4, jumpPowerStage4);
+// function clearScreen() {
+//   // Un-comment this line if you have trouble with console.clear();
+//   // return process.stdout.write('\033c');
+//   console.clear();
+// }
+
+// function sleep(milliseconds) {
+//   var start = new Date().getTime();
+//   for (var i = 0; i < 1e7; i++) {
+//     if ((new Date().getTime() - start) > milliseconds) {
+//       break;
+//     }
+//   }
+// }
+
+// function animate(boards, jumpPower) {
+//   // code here
+// }
+
+
+// // RELEASE 2
+// // animate(boards1, jumpPowerStage1);
+// // RELEASE 3
+// // animate(boards2, jumpPowerStage2);
+// // animate(boards3, jumpPowerStage3);
+// // RELEASE 4
+// // animate(boards4, jumpPowerStage4);
+
+let a = [['a','b','c','d','e'],['f','g','h','i','j']]
+
+console.log(a[a[a.length - 1]].length - 1);
