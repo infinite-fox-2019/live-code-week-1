38d09cce6ff0d52cfcf638c7d88a654b19c2c458
ItsJoel88 <imanuelnjodi@gmail.com>
2019-09-06 10:10:15 +0700

    easy release 0

--- easy/index.js
+++ easy/index.js
@@ -1,12 +1,33 @@
 // RELEASE 0
 function makeSolvent() {
   const availableIngredients = 'CFHOP';
-  // code here
+  let availableIngredients = 'CFHOP'
+  let tampung = []
+  for(let i=0; i<availableIngredients.length; i++){
+      let random = Math.floor(Math.random()*availableIngredients.length)
+      tampung.push(availableIngredients[random]+availableIngredients)
+  }
+  return tampung
+  
 }
 
 // RELEASE 1
 function checkTotalEnergy(solvent) {
-  // code here
+  let penampung = []
+  let contoh = 'HHPOO'
+  let contohSplit = contoh.split('')
+  // let result = []
+  let count = 0
+  for(let i=0; i<contoh.length; i++){
+      if(contohSplit[i] === 'H'){
+          count+=1
+          penampung.push('H'+count)
+      }else if(contohSplit[i] === 'O'){
+          count+=1
+          penampung.push('O'+count)
+      }
+  }
+  return penampung
 }
 
 // RELEASE 2
