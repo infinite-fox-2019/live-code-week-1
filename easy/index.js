5f654c6dc5c14caee6b518bdcccd10072bc471d1
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-09-06 10:07:50 +0700

    easy 80%

--- easy/index.js
+++ easy/index.js
@@ -2,16 +2,84 @@
 function makeSolvent() {
   const availableIngredients = 'CFHOP';
   // code here
+  let hasil = ''
+  let randomNum = agnkaRandom();
+  for(let i=0; i<randomNum; i++){
+    let randomI = randomIndex();
+    hasil += availableIngredients[randomI]
+  }
+  return hasil; // untuk release 0 nya random ;
 }
-
+function randomIndex () {
+  let rand = Math.floor(Math.random() * 5);
+  return rand;
+}
+function agnkaRandom () {
+  let random = Math.round(Math.random() * 10 );
+  if(random>4){
+    return random;
+  }else{
+    return agnkaRandom();
+  }
+}
+console.log(makeSolvent());
 // RELEASE 1
 function checkTotalEnergy(solvent) {
   // code here
-}
+  let kimia = makeSolvent();
+  let arr = [];
+  let count = [];
+  for(let i=0; i<kimia.length; i++){
+    if(arr.length === 0){
+      arr.push(kimia[i]);
+      count.push(1);
+    }else{
+      var counter = 0;
+      for(let j=0; j<arr.length; j++){
+        if(kimia[i] === arr[j]){
+          count[j]++;
+          counter++
+        }
+      }
+    }
+    if(counter === 0){
+      arr.push(kimia[i]);
+      count.push(1);
+    }
+  }
+  //sesuai abjad..
+  for(let i=0; i<arr.length; i++){
+    for(let j=0; j<arr.length; j++){
+      if(arr[i]<arr[j]){
+        let tamp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = tamp;
 
+        tamp = count[j];
+        count[j] = count[i];
+        count[i] = tamp;
+      }
+    }
+  }
+  let finalResult = ''
+  for(let i=0; i<arr.length; i++){
+    finalResult += count[i]+' '+arr[i]+' '
+  }
+  return finalResult  
+  // return { bahan : arr, juml : count}
+}
+// console.log(checkTotalEnergy()); // expected return 1 energy 2 H 1 O
 // RELEASE 2
 function waterSupply(needEnergy) {
   // code here
+  let kimia = checkTotalEnergy();
+  // let arr = [];
+  // while(needEnergy > 0){
+  //   if(kimia.energy === needEnergy){
+  //     arr.push(kimia)
+  //   }
+  // }
+  return kimia[1];
 }
 
 // RELEASE 3
