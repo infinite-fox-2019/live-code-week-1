816ea0d366feca0656bdacaf16746f049c68afa2
dipadana <putudipadana@gmail.com>
2019-09-06 10:22:28 +0700

    MEDIUM 50%

--- medium/snake.js
+++ medium/snake.js
@@ -1,23 +1,31 @@
 function generateSnake(row, column) {
-  let ularLurus = []
+  let ular = []
   for(let i = 0; i < row; i++){
-    ularLurus.push([]);
-  }
-  console.log(ularLurus)
-  
-  
-  
-  
-  let output = [];
-  for(let i = 0; i < row; i++){
-    output.push([]);
+    ular.push([]);
     for(let j = 0; j < column; j++){
-      output[i].push('_')
+      ular[i].push('_')
+      if(row%2 != 0 && i == row-1 && j == column-1){
+        ular[i][j] = 'o'
+      }
+      else if(row%2 == 0 && i == row-1 && j == 0){
+        ular[i][j] = 'o'
+      }
+      else if(i == 0 && j == 0){
+        ular[i][j] = '~'
+      }
     }
   }
-  return output;
+  
+  if(row == 0 && column == 0){
+    console.log('Row atau column minimal harus 1')
+  }
+  else{
+    for(let i = 0; i < row; i++){
+      console.log(ular[i].join(''))
+    }
+  }
 }
 
-let str = '~____________________________o'
+generateSnake(3,20)
 
-console.log(generateSnake(3, 6));
+// At least posisi ekor dan kepala sudah benar :)
