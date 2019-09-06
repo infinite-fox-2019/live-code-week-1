c52b482d9547b3d2527e94eda7d2be30edd84bde
risanto <potallegta@gmail.com>
2019-09-06 11:39:51 +0700

    Risanto HARD MARIO

--- hard/index.js
+++ hard/index.js
@@ -44,16 +44,37 @@ const boards4 = printBoard(stage4, lengthStage4);
 // console.log(boards4);
 
 function marioLastPosition(boards, jumpPower) {
-  boards
+  let pos = [boards.length-1, 0]
 
   for (let i = boards.length-1; i >= 0; i--) {
-    console.log(boards[i]);
     for (let j = 0; j < boards[i].length; j++) {
-      console.log(i, j);
+      if (i === boards.length-1 && j === 0) {
+        pos[0] = i;
+        pos[1] = j;
+      }
+      else if (boards[i][j] !== 'x') {
+        pos[0] = i;
+        pos[1] = j;
+
+        if (pos[1] === boards.length-1) {
+          return 'Congratulations you win the game'
+        }
+      }
+      else if (boards[i][j] === 'x') {
+        let countX = 1
+        console.log(i, j, boards[i][j]);
+
+        for (let k = i; k > 0; k--) {
+          console.log(k);
+          if (boards[k][j] === 'x') countX++
+        }
+
+        if (countX > jumpPower || countX === boards.length) {
+          return `Oops! You stuck at the step ${j-1}`
+        }
+      }
     }
   }
-
-  for ()
 }
 
 // RELEASE 1
@@ -62,9 +83,12 @@ function marioLastPosition(boards, jumpPower) {
 // const jumpPowerStage3 = 6;
 // const jumpPowerStage4 = 2;
 
-const marioLastPos1 = marioLastPosition(boards1, jumpPowerStage1);
-// const marioLastPos2 = marioLastPosition(boards2, jumpPowerStage2);
-// const marioLastPos3 = marioLastPosition(boards3, jumpPowerStage3);
+// const marioLastPos1 = marioLastPosition(boards1, jumpPowerStage1);
+// console.log(marioLastPos1);
+const marioLastPos2 = marioLastPosition(boards2, jumpPowerStage2);
+console.log(marioLastPos2);
+const marioLastPos3 = marioLastPosition(boards3, jumpPowerStage3);
+console.log(marioLastPos3);
 
 function clearScreen() {
   // Un-comment this line if you have trouble with console.clear();
