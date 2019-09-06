e10bdb3cfc9eda922c1f101d3b3c901cc15ea7fb
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-09-06 10:08:16 +0700

    commit progress

--- medium/snake.js
+++ medium/snake.js
@@ -1,5 +1,18 @@
 function generateSnake(row, column) {
-  //your code here
+  let printFromLeft = false;
+  for(let i = 0; i < row; i++) {
+    let line = '';
+    printFromLeft = !printFromLeft;
+    for(let j = 0; j < column; j++) {
+      line += '_';
+    }
+    if(printFromLeft) {
+      line = line.substring(0, line.length - 1) + '|';
+    } else {
+      line = '|' + line.substring(1, line.length);
+    }
+    console.log(line);
+  }
 }
 
 generateSnake(3, 6);
