c1005b4a76494c367048e1ae718c6bf9ef3fa19b
edwinsatya <edwinsatyayudistira@gmail.com>
2019-09-06 11:14:40 +0700

    update

--- medium/snake.js
+++ medium/snake.js
@@ -1,11 +1,31 @@
 function generateSnake(row, column) {
   //your code here
-  var result = '';
-  var jml = column * column;
+  var result = [];
+  var counter = 0;
   for (let i = 0; i < row; i++) {
-      console.log(result);
-      console.log('\n')l
+      result.push([]);
+      if (i % 2 === 0) {
+          for (let j = 0; j < column; j++) {
+              if (i === 0 && j === 0) {
+                  result[i][j] = '~';
+              } else if (j === 0) {
+                 result[i][j] = '|';
+              } else {
+                  result[i][j] = '*';
+              }
+          }
+      } else {
+          for (let j = 0; j < column; j++) {        
+                if (j === column-1) {
+                    result[i][j] = '|';
+                } else {
+                    result[i][j] = '*';
+                }
+          } 
+      }
+    }
   }
+  console.log(result);
 };
 
-(generateSnake(3, 6);
+generateSnake(3, 6);
