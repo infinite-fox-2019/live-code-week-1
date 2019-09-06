ee5e0c0a083fd6f7793331e944820db51e346e7d
hoandreas <andreas_ho@hotmail.com>
2019-09-06 10:07:25 +0700

    first commit

--- easy/index.js
+++ easy/index.js
@@ -1,17 +1,43 @@
 // RELEASE 0
-function makeSolvent() {
+function makeSolvent(length) {
+  let output = '';
   const availableIngredients = 'CFHOP';
-  // code here
+  let availIngrLength = availableIngredients.length;
+  for(let i = 0; i < length; i++) {
+    output += availableIngredients.charAt(Math.floor(Math.random() * availIngrLength));
+  }
+  return output;
 }
+// makeSolvent(5);
 
 // RELEASE 1
 function checkTotalEnergy(solvent) {
-  // code here
+  let countH = 0;
+  let countO = 0;
+  let totalEnergy = 0;
+  for(let i = 0; i < solvent.length; i++) {
+    if(solvent[i] === 'H') {
+      countH++;
+    }
+    if(solvent[i] === 'O') {
+      countO++;
+    }
+  }
+
+  if(countH >= 2 && countO >= 1) {
+    totalEnergy = 1
+  }
+
+  return totalEnergy;
 }
 
 // RELEASE 2
 function waterSupply(needEnergy) {
-  // code here
+  let solvent = makeSolvent(needEnergy);
+  let totalEnergy = checkTotalEnergy(solvent);
+  // code here 
+
+  return totalEnergy;
 }
 
 // RELEASE 3
@@ -19,4 +45,4 @@ function aggregate(solvent) {
   // code here
 }
 
-console.log(waterSupply(3));
+console.log(waterSupply(10));
