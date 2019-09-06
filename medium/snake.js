df168375553e75441ff76cbf55232934fb12c988
devitasari <devitas700@gmail.com>
2019-09-06 10:47:05 +0700

    add new file

--- medium/snake.js
+++ medium/snake.js
@@ -1,5 +1,28 @@
 function generateSnake(row, column) {
   //your code here
+  //bikin bidang kosong
+  let arr = []
+  for (let i=0; i<row; i++) {
+    arr.push([])
+    for (let j=0; j<column; j++) {
+      arr[i].push('_')
+    }
+  }
+  for (k=0; k<row; k++) {
+    if (k % 2 != 0) {
+      arr[k][column] = '|'
+    } else {
+      arr[k][0] = '|'
+    }
+  }
+  if (row % 2 == 0) {
+    arr[row-1][0] = 'o'
+  } else {
+    arr[row-1][column] = 'o'
+  }
+  arr[0][0] = '~'
+
+  return arr
 }
 
-generateSnake(3, 6);
+console.table(generateSnake(3, 20));
