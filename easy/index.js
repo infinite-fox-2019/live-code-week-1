6826f05b0e80d84e2e2656e7544ac6a4f500669b
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-09-06 11:40:08 +0700

    easy_nadhil

--- easy/index.js
+++ easy/index.js
@@ -2,11 +2,7 @@
 function makeSolvent() {
   let result = ''
   const availableIngredients = 'CFHOP';
-  // console.log(availableIngredients)
-  
-  // console.log(random)
   let randomSenyawa = Math.floor(Math.random()*11)
-  // console.log(randomSenyawa)
 
   if (randomSenyawa < 4) {
     return makeSolvent()
@@ -16,17 +12,12 @@ function makeSolvent() {
       result += availableIngredients[randomUnsur]
     }
   }
-
   return result;
-
 }
-// console.log(makeSolvent())
-
-let solvent = makeSolvent()
 
 // RELEASE 1
+let solvent = makeSolvent()
 function checkTotalEnergy(solvent) {
-console.log(solvent)
 let countH = 0
 let countO = 0
 let baseEnergy = [2,1]
@@ -42,19 +33,8 @@ countHO.push(countH)
 countHO.push(countO)
 
 let totalEnergy = countEnergy(countHO,baseEnergy)
-
-console.log(totalEnergy,'0')
-console.log(baseEnergy,"base energy",'1')
-console.log(countHO,'count HO','2')
-console.log(solvent,countH,countO,'3')
-console.log(countHO[0]-baseEnergy[0],'4')
-
-
-  // return totalEnergy
+  return {name : solvent, energy : totalEnergy}
 }
-
-console.log(checkTotalEnergy(solvent))
-
 
 function countEnergy(arr1,arr2){
   if ((arr1[0] - arr2[0]) >= 0 && (arr1[1] - arr2[1]) >= 0){
@@ -62,7 +42,6 @@ function countEnergy(arr1,arr2){
     let index1 = arr1[1]-arr2[1]
     let arrNew = []
     arrNew.push(index0,index1)
-    console.log(arrNew,'w')
     return 1 + countEnergy(arrNew,arr2)
   } else {
     return 0
@@ -71,12 +50,31 @@ function countEnergy(arr1,arr2){
 
 // RELEASE 2
 function waterSupply(needEnergy) {
-  // code here
+  let solvent = makeSolvent()
+  let energy = checkTotalEnergy(solvent).energy
+  let energyTotal = 0
+  let result = []
+  data = checkTotalEnergy(solvent)
+  result.push(data)
+  while (energyTotal < needEnergy){
+    let solvent = makeSolvent()
+    energy = checkTotalEnergy(solvent).energy
+    data = checkTotalEnergy(solvent)
+    energyTotal = energyTotal + energy
+    result.push(data)
+  }
+  return result
 }
 
 // RELEASE 3
 function aggregate(solvent) {
-  // code here
+  let senyawa = []
+  for ( let i = 0; i < solvent.length; i++){
+    senyawa.push(solvent[i].name)
+  }
+  console.log(senyawa)
 }
 
-// console.log(waterSupply(3));
+console.log(waterSupply(3));
+
+console.log(aggregate(waterSupply(3)))
