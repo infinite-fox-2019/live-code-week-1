69a62929672b77d74e4cde2f6008c1e635e5096a
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-09-06 10:07:46 +0700

    easy_nadhil

--- easy/index.js
+++ easy/index.js
@@ -1,13 +1,61 @@
 // RELEASE 0
 function makeSolvent() {
+  let result = ''
   const availableIngredients = 'CFHOP';
-  // code here
+  // console.log(availableIngredients)
+  
+  // console.log(random)
+  let randomSenyawa = Math.floor(Math.random()*11)
+  // console.log(randomSenyawa)
+
+  if (randomSenyawa < 4) {
+    return makeSolvent()
+  } else {
+    for (let i = 0; i < randomSenyawa; i++) {
+      let randomUnsur = Math.floor(Math.random()*5)
+      result += availableIngredients[randomUnsur]
+    }
+  }
+
+  return result;
+
 }
+// console.log(makeSolvent())
+
+let solvent = makeSolvent()
 
 // RELEASE 1
 function checkTotalEnergy(solvent) {
-  // code here
+console.log(solvent)
+let countH = 0
+let countO = 0
+let totalEnergy = 0
+let energy = ''
+  for (let i = 0; i< solvent.length; i++){
+    if (solvent[i] === "H"){
+      countH++
+    } else if (solvent[i] === "O"){
+      countO++
+    }
+  }
+
+  console.log(solvent,countH,countO)
+  for (let i = 0; i<countH;i++){
+    energy += "H"
+  }
+  for (let i = 0 ; i<countO;i++){
+    energy += "O"
+  }
+
+  console.log(energy)
+  
+  if (countH >= 2 && countO >= 1 ){
+    totalEnergy++
+  }
+  return totalEnergy
 }
+
+console.log(checkTotalEnergy(solvent))
 
 // RELEASE 2
 function waterSupply(needEnergy) {
@@ -19,4 +67,4 @@ function aggregate(solvent) {
   // code here
 }
 
-console.log(waterSupply(3));
+// console.log(waterSupply(3));
