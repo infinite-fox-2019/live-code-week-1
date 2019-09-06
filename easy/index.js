3840032010b3cf3eca75fca799b718b07a5fc3db
hoandreas <andreas_ho@hotmail.com>
2019-09-06 10:57:53 +0700

    second commit

--- easy/index.js
+++ easy/index.js
@@ -1,14 +1,15 @@
 // RELEASE 0
-function makeSolvent(length) {
+function makeSolvent() {
   let output = '';
   const availableIngredients = 'CFHOP';
   let availIngrLength = availableIngredients.length;
-  for(let i = 0; i < length; i++) {
+  let solventLength = 3 + (Math.floor((Math.random()*10)+1));
+  for(let i = 0; i < solventLength; i++) {
     output += availableIngredients.charAt(Math.floor(Math.random() * availIngrLength));
   }
   return output;
 }
-// makeSolvent(5);
+makeSolvent();
 
 // RELEASE 1
 function checkTotalEnergy(solvent) {
@@ -24,25 +25,37 @@ function checkTotalEnergy(solvent) {
     }
   }
 
-  if(countH >= 2 && countO >= 1) {
-    totalEnergy = 1
+  if((countH == 2 || countH == 3) && countO >= 1) {
+    totalEnergy += 1;
+  } else if((countH == 4 || countH == 5) && countO >= 2) {
+    totalEnergy += 2;
+  } else if((countH == 6 || countH == 7) && countO >= 3) {
+    totalEnergy += 3;
+  } else if((countH == 8 || countH == 9) && countO >= 4) {
+    totalEnergy += 4;
   }
-
   return totalEnergy;
 }
 
 // RELEASE 2
 function waterSupply(needEnergy) {
-  let solvent = makeSolvent(needEnergy);
-  let totalEnergy = checkTotalEnergy(solvent);
-  // code here 
-
-  return totalEnergy;
+  let output = [];
+  while(needEnergy > 0) {
+    let solvent = makeSolvent();
+    let totalEnergy = checkTotalEnergy(solvent);
+    let tempOutput = {
+      name: solvent,
+      energy: totalEnergy
+    }
+    output.push(tempOutput);
+    needEnergy--;
+  }
+  return output;
 }
 
 // RELEASE 3
 function aggregate(solvent) {
-  // code here
+
 }
 
-console.log(waterSupply(10));
+console.log(waterSupply(100));
