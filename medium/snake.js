a77e3291f8557a079f76d1f9ed647b49243ed9ac
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-09-06 10:05:32 +0700

    snake_nadhil

--- medium/snake.js
+++ medium/snake.js
@@ -1,5 +1,77 @@
 function generateSnake(row, column) {
-  //your code here
+  let outputString = "";
+  let result = [];
+
+  if (row === 1 && column === 1) {
+    return "o";
+  } else if (row === 0 || column === 0) {
+    return "Row atau column minimal 1";
+  }
+
+  for (let i = 0; i < row; i++) {
+    let temp = [];
+    for (let j = 0; j < column; j++) {
+      temp.push(j);
+    }
+    result.push(temp);
+  }
+
+  for (let i = 0; i < result.length; i++) {
+    if (row % 2 !== 0) {
+      if (i % 2 !== 0) {
+        result[i] = reverseArray(result[i]);
+      }
+    } else {
+      if (i % 2 === 0) {
+        result[i] = reverseArray(result[i]);
+      }
+    }
+  }
+  
+  if (row % 2 !== 0) {
+    for (let i = 0; i < result.length; i++) {
+      for (let j = 0; j < result[i].length; j++) {
+        if (i === 0 && j === 0) {
+          result[i][j] = "~";
+        } else if (i === result.length - 1 && j === result[i].length - 1) {
+          result[i][j] = "o";
+        } else if (result[i][j] === 0) {
+          result[i][j] = "|";
+        } else {
+          result[i][j] = "_";
+        }
+      }
+    }
+  } else if (row % 2 === 0) {
+    for (let i = 0; i < result.length; i++) {
+      for (let j = 0; j < result[i].length; j++) {
+        if (i === 0 && j === 0) {
+          result[i][j] = "~";
+        } else if (i === result.length - 1 && j === 0) {
+          result[i][j] = "o";
+        } else if (result[i][j] === 9) {
+          result[i][j] = "|";
+        } else {
+          result[i][j] = "_";
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result[i].length; j++) {
+      outputString += result[i][j];
+    }
+    outputString += "\n";
+  }
+  return outputString;
 }
 
-generateSnake(3, 6);
+function reverseArray(arr) {
+  let result = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    result.push(arr[i]);
+  }
+  return result;
+}
+console.log(generateSnake(3, 10));
