d6716c0c04e5ce582506286a563c942bf711533d
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-09-06 10:56:30 +0700

    easy_nadhil

--- easy/index.js
+++ easy/index.js
@@ -29,33 +29,45 @@ function checkTotalEnergy(solvent) {
 console.log(solvent)
 let countH = 0
 let countO = 0
-let totalEnergy = 0
-let energy = ''
-  for (let i = 0; i< solvent.length; i++){
-    if (solvent[i] === "H"){
-      countH++
-    } else if (solvent[i] === "O"){
-      countO++
-    }
+let baseEnergy = [2,1]
+let countHO = []
+for (let i = 0; i< solvent.length; i++){
+  if (solvent[i] === "H"){
+    countH++
+  } else if (solvent[i] === "O"){
+    countO++
   }
+}
+countHO.push(countH)
+countHO.push(countO)
 
-  console.log(solvent,countH,countO)
-  for (let i = 0; i<countH;i++){
-    energy += "H"
-  }
-  for (let i = 0 ; i<countO;i++){
-    energy += "O"
-  }
+let totalEnergy = countEnergy(countHO,baseEnergy)
 
-  console.log(energy)
-  
-  if (countH >= 2 && countO >= 1 ){
-    totalEnergy++
-  }
-  return totalEnergy
+console.log(totalEnergy,'0')
+console.log(baseEnergy,"base energy",'1')
+console.log(countHO,'count HO','2')
+console.log(solvent,countH,countO,'3')
+console.log(countHO[0]-baseEnergy[0],'4')
+
+
+  // return totalEnergy
 }
 
 console.log(checkTotalEnergy(solvent))
+
+
+function countEnergy(arr1,arr2){
+  if ((arr1[0] - arr2[0]) >= 0 && (arr1[1] - arr2[1]) >= 0){
+    let index0 = arr1[0]-arr2[0]
+    let index1 = arr1[1]-arr2[1]
+    let arrNew = []
+    arrNew.push(index0,index1)
+    console.log(arrNew,'w')
+    return 1 + countEnergy(arrNew,arr2)
+  } else {
+    return 0
+  }
+}
 
 // RELEASE 2
 function waterSupply(needEnergy) {
