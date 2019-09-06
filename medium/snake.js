2ba85c8261a6f989ec3b758d1301a0e9ad32b290
hoandreas <andreas_ho@hotmail.com>
2019-09-06 10:50:15 +0700

    submit live code snake

--- medium/snake.js
+++ medium/snake.js
@@ -1,5 +1,26 @@
 function generateSnake(row, column) {
-  //your code here
+  if(row < 1 && column < 1) {
+    console.log('row atau column minimal harus 1');
+  } else if(row == 1 && column == 1) {
+    console.log('o');
+  } else {
+    for(let i = 0; i < row; i++) {
+      let temp = ''
+      for(let j = 0; j < column; j++) {
+        if(i == 0 && j == 0) {
+          temp+='~';
+        } else if((j == column-1 && i % 2 !== 0) || (i % 2 == 0 && j == 0)){
+          temp+='|';
+        } else if(j === column -1 && i == row-1 || j === 0 && i == row-1) {
+          temp+='o';
+        } else {
+          temp+='_';
+        }
+      }
+      console.log(temp);
+    }
+  }
 }
 
-generateSnake(3, 6);
+generateSnake(6, 10);
+
