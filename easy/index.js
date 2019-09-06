cca850fbc74cea22ebf2187b35e8726e33426d26
mardiansn <mardiansyah.011@gmail.com>
2019-09-06 10:07:27 +0700

    commit first

--- easy/index.js
+++ easy/index.js
@@ -2,21 +2,95 @@
 function makeSolvent() {
   const availableIngredients = 'CFHOP';
   // code here
+
+  let result = ''
+  let solventLength = 4 + Math.floor(Math.random() * 7)
+  for (let i = 0; i < solventLength; i++) {
+    let randomIndex = Math.floor(Math.random() * availableIngredients.length)
+
+    result += `${availableIngredients[randomIndex]}`
+  }
+  return result
 }
 
 // RELEASE 1
 function checkTotalEnergy(solvent) {
   // code here
+  let hydroOxygen = { H: 0, O: 0, energy: 0 }
+  let compositon = {}
+  for (let i = 0; i < solvent.length; i++) {
+    if (!compositon[solvent[i]]) {
+      compositon[solvent[i]] = 1
+    } else {
+      compositon[solvent[i]]++
+    }
+  }
+
+  while (compositon['H'] >= 2 && compositon['O'] >= 1) {
+    // if (compositon['H'] >= 2 && compositon['O'] >= 1) {
+    compositon['H'] -= 2
+    compositon['O']--
+    hydroOxygen['H'] += 2
+    hydroOxygen['O'] += 1
+    hydroOxygen['energy'] += 1
+  }
+
+  return hydroOxygen
+  // return `${hydroOxygen['energy']} energy ${hydroOxygen['H']} H ${hydroOxygen['O']} O. `
+
 }
+
+// console.log(checkTotalEnergy('HHOOHHHPP'));
 
 // RELEASE 2
 function waterSupply(needEnergy) {
   // code here
+  let solvents = []
+  let gainEnergy = 0
+  while (needEnergy > gainEnergy) {
+    let ourSolvent = makeSolvent()
+    let newGenerate = checkTotalEnergy(ourSolvent)
+    solvents.push({
+      name: ourSolvent,
+      energy: newGenerate.energy
+    })
+    gainEnergy += newGenerate.energy
+  }
+
+  return solvents
 }
 
 // RELEASE 3
 function aggregate(solvent) {
-  // code here
+
+  for (let i = 0; i < solvent.length; i++) {
+    let newName = ''
+    let nameComp = {}
+    for (let j = 0; j < solvent[i].name.length; j++) {
+      let currentLetter = solvent[i].name[j]
+
+      if (!nameComp[currentLetter]) {
+        nameComp[currentLetter] = 1
+      } else {
+        nameComp[currentLetter]++
+      }
+    }
+    let orderedComp = {}
+    Object.keys(nameComp).sort().forEach(function (key) {
+      orderedComp[key] = nameComp[key];
+    });
+
+    for (let key in orderedComp) {
+      newName += `${key}${orderedComp[key]}`
+    }
+    solvent[i].name = newName
+  }
+
+  console.log(solvent);
 }
 
-console.log(waterSupply(3));
+let questions = waterSupply(3)
+console.log(questions);
+
+console.log(aggregate(questions));
+
--- medium/snake.js
+++ medium/snake.js
@@ -1,5 +1,29 @@
 function generateSnake(row, column) {
+
+  if (row < 1 || column < 1) {
+    return `row and column must be minimum of 1.`
+  } if (row === 1 && column === 1) {
+    return 'O'
+  }
+
+
+  let board = []
+  for (let i = 0; i < row; i++) {
+    let temp = []
+    for (let j = 0; j < column - 1; j++) {
+      if (i === 0) {
+        temp.push('X')
+      } if (i === (row - 1)) {
+        temp.push('O')
+      } else {
+        temp.push('#')
+      }
+    }
+    board.push(temp)
+  }
+
   //your code here
+  console.log(board);
 }
 
 generateSnake(3, 6);
