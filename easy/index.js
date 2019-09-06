8d412428314247567639c9d351369a00d78639e9
laskarks <laskar.ksatria12@gmail.com>
2019-09-05 23:07:34 -0400

    first-commit

--- easy/index.js
+++ easy/index.js
@@ -2,21 +2,119 @@
 function makeSolvent() {
   const availableIngredients = 'CFHOP';
   // code here
-}
+  let str = '';
+
+  let numRandom = 0;
+  while (numRandom < 4 || numRandom > 10) {
+    let generateRandom = Math.floor(Math.random()*11)
+    numRandom = generateRandom;
+  }
+
+  for (let i = 0;i<numRandom;i++) {
+    let rek = Math.floor(Math.random()*5);
+    str += availableIngredients[rek];
+  }
+  
+  return str
+
+}//end function makeSolvent 
+
+
 
 // RELEASE 1
 function checkTotalEnergy(solvent) {
-  // code here
-}
+    
+    let countH = 0;
+    let countO = 0;
+    for (let i = 0;i<solvent.length;i++) {
+      if (solvent[i] == 'H') {
+        countH++
+      }
+      if (solvent[i] == 'O') {
+        countO++
+      }
+    }//end loop i;
+    
 
+    let temp = Math.floor(countH/2);
+    let energyCount = 0;
+    while (temp > 0) {
+      if (countO > 0) {
+        energyCount++
+      }
+      temp--
+    };
+
+    return energyCount
+}//end function
+
+  
+  
 // RELEASE 2
 function waterSupply(needEnergy) {
-  // code here
-}
+  
+  let gather = [];
+  let limit = 0
+  while (limit < needEnergy) {
+
+    let rek = {};
+    let chemi = makeSolvent();
+    let ener = checkTotalEnergy(chemi);
+    
+    rek.name = chemi;
+    rek.energy = ener;
+    gather.push(rek);
+    limit += ener
+  }//end while
+  return gather
+}//end function watersupply
 
 // RELEASE 3
 function aggregate(solvent) {
-  // code here
-}
 
-console.log(waterSupply(3));
+  let copy = [];
+
+  for (let i = 0;i<solvent.length;i++) {
+    let s = 0;
+    for (let j = 0;j<copy.length;j++) {
+      if (solvent[i] == copy[j]) {
+        s++
+      }
+    }
+    if (s == 0) {
+      copy.push(solvent[i])
+    }
+  }
+  copy.sort();
+
+  let count = [];
+  for (let i = 0;i<copy.length;i++) {
+    let test = 0;
+    for (let j = 0;j<solvent.length;j++) {
+      if (copy[i] == solvent[j]) {
+        test++
+      }
+    }
+    count.push(test)
+  }
+
+  let str = '';
+  for (let i = 0;i<copy.length;i++) {
+    str+= copy[i];
+
+    if (count[i] > 1) {
+      str += String(count[i])
+    }
+
+  }
+  console.log(copy);
+  console.log(count)
+  return str
+
+}//end function
+
+// console.log(waterSupply(3));
+
+let agg = makeSolvent();
+console.log(agg)
+console.log(aggregate(agg))
--- medium/snake.js
+++ medium/snake.js
@@ -1,5 +1,36 @@
 function generateSnake(row, column) {
-  //your code here
-}
+  if (row == 0 || column == 0) {
+    return 'row atau column minimal harus 1'
+  }else {
+
+    let board = [];
+  board.length = row;
+
+  for (let i = 0;i<board.length;i++) {
+    let subBoard = [];
+    subBoard.length = column;
+    board[i] = subBoard
+  }
+
+  for (let i = 0;i<board.length;i++) {
+    for (let j = 0;j<board[i].length;j++) {
+      board[i][j] = '-'
+    }
+  }
+
+
+  let boardString = '';
+  for (let i = 0;i<board.length;i++) {
+    for (let j = 0;j<board[i].length;j++) {
+
+    }
+  }
+
+  return board;
+
+  }//end else
+}//end function
 
 generateSnake(3, 6);
+
+console.log(generateSnake(3,6))
