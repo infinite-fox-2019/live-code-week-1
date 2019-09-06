f54a3bc126816fdec09f7c19faddb2ba31d284ea
straf16 <depoksugutamu621@gmail.com>
2019-09-06 10:08:20 +0700

    menyelesaikan soal easy dan sedang mengerjakan soal medium

--- easy/index.js
+++ easy/index.js
@@ -2,21 +2,85 @@
 function makeSolvent() {
   const availableIngredients = 'CFHOP';
   // code here
+  let solvent = '';
+  let lengthSolvent = (Math.round(Math.random() * 6)) + 4;
+  for (let i = 0; i < lengthSolvent; i++) {
+    let randomIndex = Math.round(Math.random() * 4)
+    solvent += availableIngredients[randomIndex];
+  }
+  return solvent;
 }
 
 // RELEASE 1
 function checkTotalEnergy(solvent) {
   // code here
+  let countH = 0;
+  let countO = 0;
+  let totalEnergy = 0;
+  for (let i = 0; i < solvent.length; i++) {
+    if (solvent[i] === 'H') {
+      countH++;
+    } else if (solvent[i] === 'O') {
+      countO++;
+    }
+  }
+  while (countH >= 0 || countO >= 0) {
+    if (countH >= 2 && countO >= 1) {
+      countH -= 2;
+      countO -= 1;
+      totalEnergy++;
+    } else {
+      countH -= 2;
+      countO -= 1;
+    }
+  }
+  return totalEnergy;
 }
 
 // RELEASE 2
 function waterSupply(needEnergy) {
   // code here
+  let array = [];
+  while (needEnergy >= 0) {
+    let objSolvent = {
+      name: makeSolvent()
+    };
+    objSolvent.energy = checkTotalEnergy(objSolvent.name);
+    needEnergy -= objSolvent.energy;
+    array.push(objSolvent);
+  }
+  return array;
 }
 
 // RELEASE 3
 function aggregate(solvent) {
   // code here
+  let directoryIngredients = {
+    C: 0,
+    F: 0,
+    H: 0,
+    O: 0,
+    P: 0
+  }
+  let stringAggregate = '';
+  for (let i = 0; i < solvent.length; i++) {
+    directoryIngredients[solvent[i]]++;
+  }
+  for (let key in directoryIngredients) {
+    if (directoryIngredients[key] !== 0) {
+      if (directoryIngredients[key] === 1) {
+        stringAggregate += `${key}`;
+      } else {
+        stringAggregate += `${key}${directoryIngredients[key]}`;
+      }
+    }
+  }
+  console.log(stringAggregate);
 }
 
+let mySolvent = makeSolvent(); // console.log(mySolvent);
+let mySolventEnergy = checkTotalEnergy(mySolvent); // console.log(mySolventEnergy);
 console.log(waterSupply(3));
+aggregate(mySolvent);
+aggregate('HPCH'); // CH2P
+aggregate('HHHO'); // H30
