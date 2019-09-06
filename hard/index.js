056cf90059dcfe6a35a7b6fbd1eb4f12f991416d
devitasari <devitas700@gmail.com>
2019-09-06 11:40:21 +0700

    add new file

--- hard/index.js
+++ hard/index.js
@@ -15,6 +15,21 @@ const jumpPowerStage4 = 2;
 
 function printBoard(strTrack, lengthTrack) {
   // code here
+  let board = []
+  let a = 0
+  let b = 0
+  for (let i=0; i<strTrack.length; i += lengthTrack) {
+    board.push([])
+    for (let j=0; j<lengthTrack; j++) {
+      if (strTrack[i+j] == 'x') {
+        board[a].push('x')
+      } else {
+        board[a].push(' ')
+      }
+    }
+    a++
+  }
+  return board
 }
 
 const boards1 = printBoard(stage1, lengthStage1);
@@ -24,18 +39,36 @@ const boards4 = printBoard(stage4, lengthStage4);
 
 // RELEASE 0
 // console.log(boards1);
-// console.log(boards2);
+console.log(boards2);
 // console.log(boards3);
 // console.log(boards4);
 
 function marioLastPosition(boards, jumpPower) {
   // code here
+  let pos = [boards.length-1][0]
+  // while (jumpPower > 0) {
+  for (let i=boards.length-1; i>=0; i--) {
+    for (let j=0; j<boards[i].length; j++) {
+      if (boards[i][j] == ' ') {
+          pos = [i][j]
+      } else if (boards[i][j] == 'x') {
+          pos = [i--][j]
+          i--
+          jumpPower--
+      }
+      if (pos == [i][boards[i].length-1] && jumpPower >=0) return 'Congratulations you win the game '
+      if (jumpPower < 0) return 'Oops! You stuck at the step x'
+    }
+  }
+  // }
+  return jumpPower
 }
 
 // RELEASE 1
-// const marioLastPos1 = marioLastPosition(boards1, jumpPowerStage1);
-// const marioLastPos2 = marioLastPosition(boards2, jumpPowerStage2);
-// const marioLastPos3 = marioLastPosition(boards3, jumpPowerStage3);
+const marioLastPos1 = marioLastPosition(boards1, jumpPowerStage1);
+const marioLastPos2 = marioLastPosition(boards2, jumpPowerStage2);
+const marioLastPos3 = marioLastPosition(boards3, jumpPowerStage3);
+console.log(marioLastPos2)
 
 function clearScreen() {
   // Un-comment this line if you have trouble with console.clear();
